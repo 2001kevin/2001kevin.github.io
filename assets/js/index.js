07bf825311871f6f94089195422c2738cba38fdc
2001kevin <silahisabungan7@gmail.com>
2022-11-01 14:44:36 +0800

    solving revision

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,55 +1,58 @@
+let hour = 0;
+let minute = 0;
+let second = 0;
+let isPaused = false;
 
+function increaseHour() {
+  if (hour === 24) hour = 0;
+  else hour++;
+  $(".hour").text(hour);
+}
 
-  let hour = 0;
-  let minute = 0;
-  let second = 0;
+function decreaseHour() {
+  if (hour === 0) hour = 24;
+  else hour--;
+  $(".hour").text(hour);
+}
 
-  function increaseHour() {
-    if (hour === 24) hour = 0;
-    else hour++;
-    $(".hour").text(hour);
-  }
+function increaseMinute() {
+  if (minute === 59) minute = 0;
+  else minute++;
+  $(".minute").text(minute);
+}
 
-  function decreaseHour() {
-    if (hour === 0) hour = 24;
-    else hour--;
-    $(".hour").text(hour);
-  }
+function decreaseMinute() {
+  if (minute === 0) minute = 59;
+  else minute--;
+  $(".minute").text(minute);
+}
 
-  function increaseMinute() {
-    if (minute === 59) minute = 0;
-    else minute++;
-    $(".minute").text(minute);
-  }
+function increaseSecond() {
+  if (second === 59) second = 0;
+  else second++;
+  $(".second").text(second);
+}
 
-  function decreaseMinute() {
-    if (minute === 0) minute = 59;
-    else minute--;
-    $(".minute").text(minute);
-  }
+function decreaseSecond() {
+  if (second === 0) second = 59;
+  else second--;
+  $(".second").text(second);
+}
 
-  function increaseSecond() {
-    if (second === 59) second = 0;
-    else second++;
-    $(".second").text(second);
-  }
+function startCountdown() {
+  isPaused = false;
 
-  function decreaseSecond() {
-    if (second === 0) second = 59;
-    else second--;
-    $(".second").text(second);
-  }
+  $(".increase-btn").attr("disabled", "disabled").addClass("disable-btn");
+  $(".decrease-btn").attr("disabled", "disabled").addClass("disable-btn");
+  $("#change-btn").attr("onclick", "pauseCountdown()").html("Pause");
+  $("#stop-btn").removeAttr("disabled").removeClass("disable-btn");
+  $("#theme-btn").attr("disabled", "disabled").addClass("disable-btn");
 
-  function startCountdown() {
-    $(".increase-btn").attr("disabled", "disabled");
-    $(".decrease-btn").attr("disabled", "disabled");
-    $("#start-btn").attr("disabled", "disabled");
-    $("#stop-btn").removeAttr("disabled");
-
-    let countdown = setInterval(function () {
-      if (second <= 1) {
-        if (minute === 0) {
-          if (hour === 0) {
+  let countdown = setInterval(function () {
+    if (!isPaused) {
+      if (second < 1) {
+        if (minute <= 0) {
+          if (hour <= 0) {
             clearInterval(countdown);
             stopCountdown();
             return;
@@ -65,50 +68,68 @@
         second--;
       }
 
-      $("#tick")[0].play();
+      if (second < 10) {
+        $("#tick")[0].play();
+      }
+
+      if (minute === 2 && second === 0) {
+        $("#beep")[0].play();
+        $(".time-container").css({ "background-color": "#FF0000" });
+      }
+
       $(".second").text(second);
       $(".minute").text(minute);
       $(".hour").text(hour);
-    }, 1000);
-  }
-
-  function stopCountdown() {
-    hour = 0;
-    minute = 0;
-    second = 0;
-    let loopBeep = 5;
-    $("#beep")[0].play();
-
-    let beep = setInterval(function () {
-      if (loopBeep !== 0) {
-        $("#beep")[0].play();
-      } else {
-        clearInterval(beep);
-      }
-      console.log(loopBeep);
-      loopBeep--;
-    }, 1000);
-
-    $(".second").text(second);
-    $(".minute").text(minute);
-    $(".hour").text(hour);
-
-    $(".increase-btn").removeAttr("disabled");
-    $(".decrease-btn").removeAttr("disabled");
-    $("#start-btn").removeAttr("disabled");
-    $("#stop-btn").attr("disabled", "disabled");
-  }
-
-  function changeTheme() {
-    let attr = $("body").attr("data-theme");
-
-    if (typeof attr !== "undefined" && attr !== false) {
-      $("body").removeAttr("data-theme");
-      $(".theme-btn").html(`<i class="fa-regular fa-sun"></i>`);
-    } else {
-      $("body").attr("data-theme", "dark");
-      $(".theme-btn").html(`<i class="fa-regular fa-moon"></i>`);
     }
+  }, 1000);
+}
+
+function pauseCountdown() {
+  isPaused = true;
+  $("#change-btn").attr("onclick", "resumeCountdown()").html("Resume");
+}
+
+function resumeCountdown() {
+  isPaused = false;
+  $("#change-btn").attr("onclick", "pauseCountdown()").html("Pause");
+}
+
+function stopCountdown() {
+  hour = 0;
+  minute = 0;
+  second = 0;
+  let loopBeep = 4;
+  $("#beep")[0].play();
+
+  let beep = setInterval(function () {
+    if (loopBeep !== 0) {
+      $("#beep")[0].play();
+    } else {
+      clearInterval(beep);
+    }
+    loopBeep--;
+  }, 1000);
+
+  $(".second").text(second);
+  $(".minute").text(minute);
+  $(".hour").text(hour);
+  $(".time-container").css({ backgroundColor: "var(--color-1)" });
+
+  $(".increase-btn").removeAttr("disabled").removeClass("disable-btn");
+  $(".decrease-btn").removeAttr("disabled").removeClass("disable-btn");
+  $("#change-btn").attr("onclick", "startCountdown()").html("Start");
+  $("#stop-btn").attr("disabled", "disabled").addClass("disable-btn");
+  $("#theme-btn").removeAttr("disabled").removeClass("disable-btn");
+}
+
+function changeTheme() {
+  let attr = $("body").attr("data-theme");
+
+  if (typeof attr !== "undefined" && attr !== false) {
+    $("body").removeAttr("data-theme");
+    $(".theme-btn").html(`<i class="fa-regular fa-sun"></i>`);
+  } else {
+    $("body").attr("data-theme", "dark");
+    $(".theme-btn").html(`<i class="fa-regular fa-moon"></i>`);
   }
-
-
+}
